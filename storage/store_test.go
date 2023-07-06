87fdbd5e12b16183d8e20d04b84d0f5ed4d7bfa1
tauraamui <adampstringer@protonmail.com>
2023-07-06 15:39:37 +0100

    test: ensure PKs per table increment separately correctly

--- storage/store_test.go
+++ storage/store_test.go
@@ -18,6 +18,16 @@ func (b Balloon) TableName() string { return "balloons" }
 func (b *Balloon) SetID(id uint32)  { b.ID = id }
 func (b *Balloon) Ref() any         { return b }
 
+type Cake struct {
+	ID       uint32 `mdb:"ignore"`
+	Type     string
+	Calories int
+}
+
+func (b Cake) TableName() string { return "cakes" }
+func (b *Cake) SetID(id uint32)  { b.ID = id }
+func (b *Cake) Ref() any         { return b }
+
 func TestStoreNewSuccess(t *testing.T) {
 	is := is.New(t)
 
@@ -38,6 +48,9 @@ func TestStoreBalloonsSuccess(t *testing.T) {
 
 	store := storage.New(db)
 	is.True(store != nil)
+	defer func() {
+		is.NoErr(store.Close())
+	}()
 
 	bigRedBalloon := Balloon{Color: "RED", Size: 695}
 	smallYellowBalloon := Balloon{Color: "YELLOW", Size: 112}
@@ -49,4 +62,40 @@ func TestStoreBalloonsSuccess(t *testing.T) {
 	is.Equal(bigRedBalloon.ID, uint32(0))
 	is.Equal(smallYellowBalloon.ID, uint32(1))
 	is.Equal(mediumWhiteBalloon.ID, uint32(2))
+}
+
+func TestStoreBalloonsAndCakesInSuccessionRetainsCorrectRowIDs(t *testing.T) {
+	is := is.New(t)
+
+	db, err := kvs.NewMemKVDB()
+	is.NoErr(err)
+	defer db.Close()
+
+	store := storage.New(db)
+	is.True(store != nil)
+	defer func() {
+		is.NoErr(store.Close())
+	}()
+
+	bigRedBalloon := Balloon{Color: "RED", Size: 695}
+	disguistingVeganCake := Cake{Type: "INEDIBLE", Calories: -38}
+	smallYellowBalloon := Balloon{Color: "YELLOW", Size: 112}
+	healthyishCarrotCake := Cake{Type: "CARROT", Calories: 280}
+	mediumWhiteBalloon := Balloon{Color: "WHITE", Size: 366}
+	redVelvetCake := Cake{Type: "RED_VELVET", Calories: 410}
+
+	is.NoErr(store.Save(kvs.RootOwner{}, &bigRedBalloon))
+	is.NoErr(store.Save(kvs.RootOwner{}, &smallYellowBalloon))
+	is.NoErr(store.Save(kvs.RootOwner{}, &mediumWhiteBalloon))
+
+	is.NoErr(store.Save(kvs.RootOwner{}, &disguistingVeganCake))
+	is.NoErr(store.Save(kvs.RootOwner{}, &healthyishCarrotCake))
+	is.NoErr(store.Save(kvs.RootOwner{}, &redVelvetCake))
+
+	is.Equal(bigRedBalloon.ID, uint32(0))
+	is.Equal(disguistingVeganCake.ID, uint32(0))
+	is.Equal(smallYellowBalloon.ID, uint32(1))
+	is.Equal(healthyishCarrotCake.ID, uint32(1))
+	is.Equal(mediumWhiteBalloon.ID, uint32(2))
+	is.Equal(redVelvetCake.ID, uint32(2))
 }

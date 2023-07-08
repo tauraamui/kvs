2f1d57ae7e343fffebc1767b5f50d4d622c0b3e7
tauraamui <adampstringer@protonmail.com>
2023-07-08 08:59:14 +0100

    include more examples

---
+++ example/hiarchy.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/tauraamui/kvs"
+	"github.com/tauraamui/kvs/storage"
+)
+
+type SmallChild struct {
+	UUID         kvs.UUID
+	HungryMetric uint32
+	Norished     bool
+}
+
+type Cake struct {
+	ID       uint32 `mdb:"ignore"`
+	Type     string
+	Calories int
+}
+
+func (b Cake) TableName() string { return "cakes" }
+func (b *Cake) SetID(id uint32)  { b.ID = id }
+func (b *Cake) Ref() any         { return b }
+
+func hiarchy() {
+	db, err := kvs.NewMemKVDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	store := storage.New(db)
+	defer store.Close()
+
+	child := SmallChild{
+		UUID:         uuid.New(),
+		HungryMetric: 100,
+	}
+
+	disguistingVeganCake := Cake{Type: "INEDIBLE", Calories: -38}
+	healthyishCarrotCake := Cake{Type: "CARROT", Calories: 280}
+	redVelvetCake := Cake{Type: "RED_VELVET", Calories: 410}
+
+	store.Save(child.UUID, &disguistingVeganCake)
+	store.Save(child.UUID, &healthyishCarrotCake)
+	store.Save(child.UUID, &redVelvetCake)
+
+	bs, err := storage.LoadAllByOwner(store, Cake{}, child.UUID)
+	for _, cake := range bs {
+		fmt.Printf("ROWID: %d, %+v\n", cake.ID, cake)
+	}
+}

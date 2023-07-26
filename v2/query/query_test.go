2d1f334b7a54eb4c3d501aeabbe47cb359bc8a2d
tauraamui <adampstringer@protonmail.com>
2023-07-26 23:58:12 +0100

    test: ensure single predicate with multi values works

--- v2/query/query_test.go
+++ v2/query/query_test.go
@@ -49,6 +49,32 @@ func TestQueryFilterWithSinglePredicateSuccess(t *testing.T) {
 	is.Equal(bs[0].Size, 695)
 }
 
+func TestQueryFilterWithSinglePredicateMultipleValuesSuccess(t *testing.T) {
+	is := is.New(t)
+
+	db, err := kvs.NewMemKVDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	store := storage.New(db)
+	defer store.Close()
+
+	store.Save(kvs.RootOwner{}, &Balloon{Color: "WHITE", Size: 366})
+	store.Save(kvs.RootOwner{}, &Balloon{Color: "RED", Size: 695})
+
+	bs, err := query.Run[Balloon](store, query.New().Filter("color").Eq("GREEN", "WHITE", "CYAN", "PURPLE", "RED", "GOLD"))
+	is.NoErr(err)
+	is.Equal(len(bs), 2)
+	is = is.NewRelaxed(t)
+	is.Equal(bs[0].Color, "WHITE")
+	is.Equal(bs[0].Size, 366)
+
+	is.Equal(bs[1].Color, "RED")
+	is.Equal(bs[1].Size, 695)
+}
+
 func TestQueryFilterWithSinglePredicateFailure(t *testing.T) {
 	is := is.New(t)
 
@@ -105,6 +131,29 @@ func TestQueryFilterWithMultiplePredicateSuccess(t *testing.T) {
 	is.Equal(bs[0].Size, 695)
 }
 
+func TestQueryFilterWithMultiplePredicateMultipleValuesSuccess(t *testing.T) {
+	is := is.New(t)
+
+	db, err := kvs.NewMemKVDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	store := storage.New(db)
+	defer store.Close()
+
+	store.Save(kvs.RootOwner{}, &Balloon{Color: "RED", Size: 695})
+	store.Save(kvs.RootOwner{}, &Balloon{Color: "WHITE", Size: 366})
+
+	bs, err := query.Run[Balloon](store, query.New().Filter("color").Eq("ZIMA_BLUE", "BLACK", "WHITE", "RED").Filter("size").Eq(222, 366, 948))
+	is.NoErr(err)
+	is.Equal(len(bs), 1)
+	is = is.NewRelaxed(t)
+	is.Equal(bs[0].Color, "WHITE")
+	is.Equal(bs[0].Size, 366)
+}
+
 func TestQueryFilterWithMultiplePredicateFailure(t *testing.T) {
 	is := is.New(t)
 
@@ -125,6 +174,26 @@ func TestQueryFilterWithMultiplePredicateFailure(t *testing.T) {
 	is.Equal(len(bs), 0)
 
 	bs, err = query.Run[Balloon](store, query.New().Filter("color").Eq("RED").Filter("size").Eq(548))
+	is.NoErr(err)
+	is.Equal(len(bs), 0)
+}
+
+func TestQueryFilterWithMultiplePredicateMultipleValuesFailure(t *testing.T) {
+	is := is.New(t)
+
+	db, err := kvs.NewMemKVDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	store := storage.New(db)
+	defer store.Close()
+
+	store.Save(kvs.RootOwner{}, &Balloon{Color: "RED", Size: 695})
+	store.Save(kvs.RootOwner{}, &Balloon{Color: "WHITE", Size: 366})
+
+	bs, err := query.Run[Balloon](store, query.New().Filter("color").Eq("GREY", "PINK").Filter("size").Eq(110, 366))
 	is.NoErr(err)
 	is.Equal(len(bs), 0)
 }
